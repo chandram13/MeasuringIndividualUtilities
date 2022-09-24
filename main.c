7a912da121862563a55666e323ac6ea4b44f7db0
chandram13 <mschandr@uci.edu>
2022-09-24 08:52:58 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <assert.h>
+
+// Marvish Chandra
+// Measuring Individual Utilities
+
+static void differingUtilities(uA,sA,xA,uB,sB,xB,uC,xC,sC){
+    assert = xA + 1/2 * sA; // biocentric
+    assert = xB + uA; // alutristic
+    assert = xC + sA + sB + sC; // paternalistic
+    return 0;
+}
